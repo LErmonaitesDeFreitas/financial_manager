c0624e5e200e9fc1a153ece4e3168ddaae36456d
LErmonaitesDeFreitas <lincolnermonaites@gmail.com>
2018-10-24 22:35:26 -0300

    Melhorias no token

--- frontend/token.js
+++ frontend/token.js
@@ -1,14 +1,35 @@
 
-tokenIsValid = (callback) => {
+tokenIsValid = (myCallback) => {
     try {
         var host = "http://localhost:3001";
         var user = JSON.parse(localStorage.user);
         var endpoint = host + "/token/verify/" + user.token;
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = callback
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                myCallback(JSON.parse(this.responseText).isValid);
+            }
+        }
         xhttp.open("POST", endpoint, true);
         xhttp.send();
     } catch (e) {
 
     }
 }
+tokenIsValid(function (isValid) {
+    var route = location.pathname;
+    route = route.split("/")[1];    
+                                        var redirect = "";
+    switch (route) {
+        case "login":
+            if (isValid)
+                redirect = "dashboard";
+            break;
+        case "dashboard":
+            if (!isValid)
+                redirect = "login";
+            break;
+    }
+    if (redirect != route)
+        location.pathname = "/" + redirect;
+});
