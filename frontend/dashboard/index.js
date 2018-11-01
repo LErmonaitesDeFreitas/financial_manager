d87b5476a13599c5a73ce6444b89ae753eef62c0
LErmonaitesDeFreitas <lincolnermonaites@gmail.com>
2018-11-01 20:34:08 -0300

    Cards

--- frontend/dashboard/index.js
+++ frontend/dashboard/index.js
@@ -2,7 +2,7 @@ var app = angular.module("app", []);
 app.controller("ctrl", function ($scope, $http) {
     var data = JSON.parse(localStorage.user);
     $scope.user = data.user;
-    console.log(data);
+    $scope.currentMonth = data.currentMonth;
 });
 
 
